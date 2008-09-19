35f10bb287fb2edcf3c2e871094e20aded94a6d1
Dan Barry <dan@chaptercommunications.com>
2008-09-19 07:54:54 -0500

    start workaround

--- tweets.php
+++ tweets.php
@@ -1,9 +1,9 @@
 <?php
-header('Content-Type: application/atom+xml; charset=utf-8');
+//header('Content-Type: application/atom+xml; charset=utf-8');
 
 $parameters = array(
   'appid' => 'tCa4.fDV34GcXMnIbz5dWxSZWjSWsMXIQSqiVVBFb55nTKX1RFLqYyuM677Jz2vK',
-  'context' => $_GET['description']
+  'context' => file_get_contents($_GET['identifier'])
 );
 
 $joined_parameters = array();
@@ -12,7 +12,15 @@ foreach ($parameters as $key => $value)
 
 $url_parameters = '?' . implode('&', $joined_parameters);
 
-$keyword_xml = simplexml_load_file('http://search.yahooapis.com/ContentAnalysisService/V1/termExtraction' . $url_parameters);
+$keyword_request = curl_init();
+curl_setopt($keyword_request, CURLOPT_URL, 'http://search.yahooapis.com/ContentAnalysisService/V1/termExtraction');
+curl_setopt($keyword_request, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($keyword_request, CURLOPT_POST, 1);
+curl_setopt($keyword_request, CURLOPT_POSTFIELDS, $url_parameters);
+$result = curl_exec($keyword_request);
+echo $result;
+$keyword_xml = simplexml_load_string($result);
+curl_close($keyword_request);
 
 $keywords = array();
 
